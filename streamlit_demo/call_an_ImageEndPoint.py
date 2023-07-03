046c292fb3977066e355773f8a8122d55b770d4f
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-07-02 23:59:29 -0700

    Feat(add) : added the fetch code for an image pull

--- streamlit_demo/call_an_ImageEndPoint.py
+++ streamlit_demo/call_an_ImageEndPoint.py
@@ -7,3 +7,22 @@ def fetch(session, url):
         return result
     except Exception:
         return {}
+    
+def main():
+    st.set_page_config("Image viewer page")
+    st.title("Image Viewer Demo Page")
+    session = requests.Session()
+    with st.form("my-form"):
+        index = st.number_input("ID", min_value=0, max_value=100, key="index")
+        submitted=st.form_submit_button("Submit")
+
+        if submitted:
+            st.write("Result")
+            data = fetch(session, f"https://picsum.photos/id/{index}/info")
+            if data:
+                st.image(data['download_url'], caption=f"Author: {data['author']}")
+            else:
+                st.error("Error")
+
+if __name__ == "__main__":
+    main()
