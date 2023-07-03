e28b30f2347012f4d6399fa91feaa080202ea337
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-07-03 04:28:50 -0700

    Feat(add): added some contents

--- streamlit_demo/call_an_ImageEndPoint.py
+++ streamlit_demo/call_an_ImageEndPoint.py
@@ -1,32 +1,33 @@
 import streamlit as st
-# import requests
-import httpx
+import aiohttp
+import asyncio
 
-def fetch(session, url):
+async def fetch(session, url):
     try:
-        result = session.get(url)
-        return result
+        async with session.get(url) as response:
+            result = await response.json()
+            return result
     except Exception:
         return {}
     
-def main():
+async def main():
     st.set_page_config("Image viewer page")
     st.title("Image Viewer Demo Page")
-    session = httpx.Client()
+    
+    async with aiohttp.ClientSession() as session:
+        with st.form("my-form"):
+            index = st.number_input("ID", min_value=0, max_value=100, key="index")
+            submitted = st.form_submit_button("Submit")
 
-    with st.form("my-form"):
-        index = st.number_input("ID", min_value=0, max_value=100, key="index")
-        submitted=st.form_submit_button("Submit")
+            if submitted:
+                st.write("Result")
+                data = await fetch(session, f"https://picsum.photos/id/{index}/info")
 
-        if submitted:
-            st.write("Result")
-            data = fetch(session, f"https://picsum.photos/id/{index}/info")
-            if data:
-                image = data['download_url']
-                author = data['author']
-                st.image(image, caption='My Image Please Work')
-            else:
-                st.error("Error")
+                if data:
+                    st.image(data['download_url'], caption=f"Author: {data['author']}")
+                else:
+                    st.error("Error")
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
