02dac4dff77855a4ad5af4d90c258ba00f2462b1
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-07-05 11:44:37 -0700

    Feat(add): added a placeholder code for scheduled model inference

---
+++ main_app/streamlit_client/pages/SCHEDULED-INFERENCE.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.markdown("<h1 style= 'color: green;'>Scheduled Model Inference</h1>", unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
