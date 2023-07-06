4d439b14877fb45a9c9b8f82d6e1a74a2a616b41
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-07-06 01:56:07 -0700

    Feat(add): added some code

--- main_app/streamlit_client/pages/CUSTOM-INFERENCE.py
+++ main_app/streamlit_client/pages/CUSTOM-INFERENCE.py
@@ -6,9 +6,14 @@ import os
 def main():
     st.markdown("<h1 style= 'color: green;'>Custom Model Inference</h1>", unsafe_allow_html=True)
     dataset_file = st.file_uploader("Upload a Custom Dataset(.csv)", type=["csv"])
-    url = "https://file.io/"
+    url = "https://uploadfile.io/upload"
+    # NEEDS SOME LOGIC FOR DATA AND WIDGET PERSISTANCE ON STREAMLIT
+    # if 'dataset_file' not in st.session_state:
+    #     st.session_state.dataset_file = pd.DataFrame()
+
     if dataset_file is not None:
         df = pd.read_csv(dataset_file)
+        # st.session_state.dataset_file = df
         st.dataframe(df)
         df.to_csv('custom.csv')
         submit = st.button("Run Model Inference")
@@ -18,7 +23,7 @@ def main():
 
             if os.path.exists("custom.csv"):
                 os.remove("custom.csv")
-                
+            st.write(dir(response))    
             st.write(response.json()['link'])
 
     
