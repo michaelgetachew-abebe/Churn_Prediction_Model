44302a0cb951844482878f3319caf1efa530d461
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-07-09 16:25:54 -0700

    Feat(add): added a test api batch serving

---
+++ main_app/batch_inference/test.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("path/to/saved/model.pkl")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Load the CSV data from the request
+    csv_data = request.data.decode("utf-8")
+    df = pd.read_csv(StringIO(csv_data))
+
+    # Use the loaded model to make predictions on the CSV data
+    predictions = model.predict(df)
+
+    # Return the predictions as a JSON response
+    return {"predictions": predictions.tolist()}
+
+if __name__ == "__main__":
+    app.run()
