f7b5ac079af8906717fe71ce6b425941ad125bb0
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-07-09 19:04:41 -0700

    Feat(add): added some code and got near somewhere

--- main_app/streamlit_client/pages/CUSTOM-INFERENCE.py
+++ main_app/streamlit_client/pages/CUSTOM-INFERENCE.py
@@ -1,11 +1,21 @@
 import streamlit as st
 import pandas as pd
 import requests
+from sklearn.feature_extraction import DictVectorizer
+import json
+import numpy as np
 
 def main():
     st.markdown("<h1 style= 'color: green;'>Custom Model Inference</h1>", unsafe_allow_html=True)
     dataset_file = st.file_uploader("Upload a Custom Dataset(.csv)", type=["csv"])
-    url = 'https://filesharego.com/upload'
+    url = 'http://127.0.0.1:9696/predict'
+    dv  = DictVectorizer(sparse=False)
+    categorical = ['gender', 'seniorcitizen', 'partner', 'dependents',
+               'phoneservice', 'multiplelines', 'internetservice',
+               'onlinesecurity', 'onlinebackup', 'deviceprotection',
+               'techsupport', 'streamingtv', 'streamingmovies',
+               'contract', 'paperlessbilling', 'paymentmethod']
+    numerical = ['tenure', 'monthlycharges', 'totalcharges']
     # NEEDS SOME LOGIC FOR DATA AND WIDGET PERSISTANCE ON STREAMLIT
     # if 'dataset_file' not in st.session_state:
     #     st.session_state.dataset_file = pd.DataFrame()
@@ -13,14 +23,21 @@ def main():
     if dataset_file is not None:
         df = pd.read_csv(dataset_file)
         st.dataframe(df)
-        files = {'file': dataset_file.read()}        
+
+        df = df[categorical + numerical].to_dict(orient = 'records')
+        dv.fit(df)
+
+        df = dv.transform(df)
         
+        # files = {'file': dataset_file.read()}        
+        
+        json_data = json.dumps(np.ndarray.tolist(df))
         submit = st.button("Run Model Inference")
         if submit:
-            response = requests.post(url, files=files)    
-            st.write(dir(response))
+            response = requests.post(url, json=json_data)    
+            #st.write(dir(response))
             st.write(response.json())
-            st.write(response.json()['link'])
+            #st.write(response.json()['link'])
 
     
 if __name__ == "__main__":
