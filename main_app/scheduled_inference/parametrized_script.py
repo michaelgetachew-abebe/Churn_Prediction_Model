c50bdac473976e410c0f9477db7d62b4f0be932b
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-07-11 17:51:49 -0700

    Feat(add): started adding code for offline deployment going on

---
+++ main_app/scheduled_inference/parametrized_script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+import random
+
+import seaborn as sns
+from matplotlib import pyplot as pyplot
+
+import mlflow
+# SOME AIRFLOW REALTED THINGS
+
+import pickle
+from sklearn.feature_extraction import DictVectorizer
